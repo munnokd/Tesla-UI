9493bbdf5e02b1aef20124dbcd30fea736f5382d
Kalp Prajapati <81804004+munnokd@users.noreply.github.com>
2021-08-11 15:21:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="app">
+      <Header/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
