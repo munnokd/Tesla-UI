b2c6e306ca3139793306e7d64f2bac2cf2bafc3b
Kalp Prajapati <81804004+munnokd@users.noreply.github.com>
2021-08-11 15:22:39 +0530

    Add files via upload

--- src/app/store.js
+++ src/app/store.js
@@ -1,1 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import carReducer from "../features/carApp/carSlice"
 
+export const store = configureStore({
+  reducer: {
+    car:carReducer
+  },
+});
