832f0e00f0f8e319fe5d2bf0a71e81485db51481
Kalp Prajapati <81804004+munnokd@users.noreply.github.com>
2021-08-11 15:24:28 +0530

    Add files via upload

--- features/carApp/carSlice.js
+++ features/carApp/carSlice.js
@@ -1,1 +1,13 @@
+import {createSlice} from '@reduxjs/toolkit'
 
+const initialState={
+    cars:["Model S","Model Y","Model 3","Model X","solr Panels","Solar Roof"]
+}
+const carSlice=createSlice({
+    name:"car",
+    initialState,
+    reducers:{}
+})
+
+export const selectCars=state=>state.car.cars
+export default carSlice.reducer
