7ac04b7559f0dcc94783fbe60bee46978baf542d
Harsh meghani <harshmeghani@Solvatives-MacBook-Air-5.local>
2023-03-29 16:45:27 +0530

    smooth scroll navbar isssue solved

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,58 +1,69 @@
-import React, { useState } from 'react'
-import './Header.css'
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import CloseIcon from '@material-ui/icons/Close';
-import { selectCars } from '../features/carApp/carSlice'
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import "./Header.css";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import CloseIcon from "@material-ui/icons/Close";
+import { selectCars } from "../features/carApp/carSlice";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-    const [close, setClose] = useState(false);
-    const cars = useSelector(selectCars)
+  const [close, setClose] = useState(false);
+  const cars = useSelector(selectCars);
 
-    return (
-        <div className="header">
-            <a className="header__logo">
-                <img src="/Images/logo.svg" alt="" />
+  return (
+    <div className="header">
+      <a className="header__logo">
+        <img src="/Images/logo.svg" alt="" />
+      </a>
+      <div className="header__list">
+        {cars &&
+          cars.map((car, index) => (
+            <a key={index} href={`#${car}`}>
+              {car}
             </a>
-            <div className="header__list">
-                {cars && cars.map((car, index) => (
-                    <a key={index} href="#" >{car}</a>
-                ))}
-            </div>
-            <div className="header__menu">
-                <div className="header__menu__a">
-                    <a href="#" >Shop</a>
-                    <a href="#" >Account</a>
-                </div>
-                <p className="header__menu__1" onClick={() => setClose(true)}  >Menu</p>
-            </div>
-            {close ?
-                <div className="header__menu__list"    >
-                    <div className="header__menu__close">
-                        <CloseIcon fontSize="large" onClick={() => setClose(false)} />
-                    </div>
-                    <div className="header__menu__item">
-                        {cars && cars.map((car, index) => (
-                            <a key={index} href="#" >{car}</a>
-                        ))}
-                    </div>
-                    <a href="#">Existing Inventory</a>
-                    <a href="#">Used Inventory</a>
-                    <a href="#">Trade-In</a>
-                    <a href="#">Test Drive</a>
-                    <a href="#">Powerwall</a>
-                    <a href="#">Commercial Energy</a>
-                    <a href="#">Utilities</a>
-                    <a href="#">Charging</a>
-                    <a href="#">Find Us</a>
-                    <a href="#">Support</a>
-                    <a href="#">Investor Relations</a>
-                    <a href="#">Account</a>
-                    <a className="header__menu__icon" href="#">More<ChevronRightIcon fontSize="small" /></a>
-                </div> : null
-            }
+          ))}
+      </div>
+      <div className="header__menu">
+        <div className="header__menu__a">
+          <a href="#">Shop</a>
+          <a href="#">Account</a>
         </div>
-    )
-}
+        <p className="header__menu__1" onClick={() => setClose(true)}>
+          Menu
+        </p>
+      </div>
+      {close ? (
+        <div className="header__menu__list">
+          <div className="header__menu__close">
+            <CloseIcon fontSize="large" onClick={() => setClose(false)} />
+          </div>
+          <div className="header__menu__item">
+            {cars &&
+              cars.map((car, index) => (
+                <a key={index} href={`#${car}`}>
+                  {car}
+                </a>
+              ))}
+          </div>
+          <a href="#">Existing Inventory</a>
+          <a href="#">Used Inventory</a>
+          <a href="#">Trade-In</a>
+          <a href="#">Test Drive</a>
+          <a href="#">Powerwall</a>
+          <a href="#">Commercial Energy</a>
+          <a href="#">Utilities</a>
+          <a href="#">Charging</a>
+          <a href="#">Find Us</a>
+          <a href="#">Support</a>
+          <a href="#">Investor Relations</a>
+          <a href="#">Account</a>
+          <a className="header__menu__icon" href="#">
+            More
+            <ChevronRightIcon fontSize="small" />
+          </a>
+        </div>
+      ) : null}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,64 +1,78 @@
-import React from 'react'
-import './Home.css'
-import Part from './Part'
+import React from "react";
+import "./Home.css";
+import Part from "./Part";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <Part ani={"ani"} 
-                title={"Model S"}  
-                desc="Oreder Online for "
-                descun="Touchless Delivery"
-                bgimg={"/Images/models/model-s.jpg"}
-                button1="Custom Order"
-                button2="Existing Inventory"
-            />
-            <Part
-                title="Model Y"  
-                desc="Oreder Online for "
-                descun="Touchless Delivery"
-                bgimg={"/Images/modely/model-y.jpg"}
-                button1="Custom Order"
-                button2="Existing Inventory"
-            />
-            <Part
-                title="Model 3"  
-                desc="Oreder Online for "
-                descun="Touchless Delivery"
-                bgimg={"/Images/model3/model-3.jpg"}
-                button1="Custom Order"
-                button2="Existing Inventory"
-            />
-            <Part
-                title="Model X"  
-                desc="Oreder Online for "
-                descun="Touchless Delivery"
-                bgimg={"Images/modelx/model-x.jpg"}
-                button1="Custom Order"
-                button2="Existing Inventory"
-            />
-            <Part
-                title="Solar Panels"  
-                desc="Lowest Cost Solar Panels in America"
-                bgimg={"/Images/solar-panels/solar-panel.jpg"}
-                button1="Order Now"
-                button2="Learn more"
-            />
-            <Part
-                title="Solar Roof"  
-                desc="Produce Clean Energy From Your Roof"
-                bgimg={"/Images/solar-roof/solar-roof.jpg"}
-                button1="Order Now"
-                button2="Learn more"
-            />
-            
-            <Part
-                title="Accessories"  
-                bgimg={"/Images/accessories/accessories.jpg"}
-                button1="shop now"
-            />
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <section id="Model S">
+        <Part
+          ani={"ani"}
+          title={"Model S"}
+          desc="Oreder Online for "
+          descun="Touchless Delivery"
+          bgimg={"/Images/models/model-s.jpg"}
+          button1="Custom Order"
+          button2="Existing Inventory"
+        />
+      </section>
+      <section id="Model Y">
+        <Part
+          title="Model Y"
+          desc="Oreder Online for "
+          descun="Touchless Delivery"
+          bgimg={"/Images/modely/model-y.jpg"}
+          button1="Custom Order"
+          button2="Existing Inventory"
+        />
+      </section>
+      <section id="Model 3">
+        <Part
+          title="Model 3"
+          desc="Oreder Online for "
+          descun="Touchless Delivery"
+          bgimg={"/Images/model3/model-3.jpg"}
+          button1="Custom Order"
+          button2="Existing Inventory"
+        />
+      </section>
+      <section id="Model X">
+        <Part
+          title="Model X"
+          desc="Oreder Online for "
+          descun="Touchless Delivery"
+          bgimg={"Images/modelx/model-x.jpg"}
+          button1="Custom Order"
+          button2="Existing Inventory"
+        />
+      </section>
+      <section id="solr Panels">
+        <Part
+          title="Solar Panels"
+          desc="Lowest Cost Solar Panels in America"
+          bgimg={"/Images/solar-panels/solar-panel.jpg"}
+          button1="Order Now"
+          button2="Learn more"
+        />
+      </section>
+      <section id="Solar Roof">
+        <Part
+          title="Solar Roof"
+          desc="Produce Clean Energy From Your Roof"
+          bgimg={"/Images/solar-roof/solar-roof.jpg"}
+          button1="Order Now"
+          button2="Learn more"
+        />
+      </section>
+      <section id="Accessories">
+        <Part
+          title="Accessories"
+          bgimg={"/Images/accessories/accessories.jpg"}
+          button1="shop now"
+        />
+      </section>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
